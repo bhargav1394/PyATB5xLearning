e6e758cfaeede537bce94c97a61262a4aa93c8db
Bhargav.Rathod <bhargav.rathod13@gmail.com>
2024-12-13 14:14:07 +0530

    Update files

---
+++ Nov/ex_26112024_Dict/LAB_102_Dictionory_Frequency_chr_string.py
@@ -0,0 +1,15 @@
+#Frequency of characters in a string
+# Write a program to count the frequency of each charcter in a given string.
+#ans {a:2}
+string = "automation"
+string1 = input("Enter the input e.g automation")
+
+char_count = {}
+
+#Logic Building
+#I/P -> String
+#O/P -> dict
+
+for char in string:
+    char_count[char] = char_count.get(char,0) + 1
+print(char_count)
